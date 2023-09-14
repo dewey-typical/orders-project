b65eb74a4b44f4bbb379cfd1bee8f7e7a802168f
dewey-typical <touil.anas.lpcb@gmail.com>
2023-09-14 17:31:36 +0200

    add application package

--- main.go
+++ main.go
@@ -1,28 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/dewey-typical/orders-api/application"
 )
 
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
+	app := application.New()
 
-	router.Get("/hello", basicHandler)
-	//fmt.Println("Hello")
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
-
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("failed to listen to server", err)
+		fmt.Println("failed to start app:", err)
 	}
 }
 
